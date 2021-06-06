a44c389c0e7962e5fac1fc29846ff50197a2be70
superherointj <5861043+superherointj@users.noreply.github.com>
2021-06-06 16:21:39 -0300

    k

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  #inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  #inputs.nixpkgs.url = "github:superherointj/nixpkgs/package-ocamlPackages-ppx_gen_rec-2";
+  #inputs.nixpkgs.url = "/home/intj/git/nixpkgs/";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
 
   outputs = { self, nixpkgs, ... }@inputs:
     {
